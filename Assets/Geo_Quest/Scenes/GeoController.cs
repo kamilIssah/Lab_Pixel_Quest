31c48c82cdbd3aae7a76f620f39eaed008f6572e
kamilIssah <kamilmissah@gmail.com>
2025-07-10 12:16:56 -0400

    Movement input keys

---
+++ Assets/Geo_Quest/Scenes/GeoController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+    }
+    
+    // Update is called once per frame
+    void Update()
+    // Movement Keys
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.position += new Vector3(0, 1, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.position += new Vector3(-1, 0, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            transform.position += new Vector3(0, -1, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.position += new Vector3(1, 0, 0);
+        }
+    }
+   
+}
