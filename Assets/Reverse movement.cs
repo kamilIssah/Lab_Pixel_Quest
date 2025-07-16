61db6cc4fcd25a4198fa207e0fd9ac5d0b5268a0
kamilIssah <kamilmissah@gmail.com>
2025-07-16 12:11:19 -0400

    Pixel game

---
+++ Assets/Reverse movement.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Reversemovement : MonoBehaviour
+{
+    int var = 3;
+    private Rigidbody2D rb;
+    public int speed = -5;
+    public string nextlevel = "Scene_2";
+    private SpriteRenderer sr;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(xInput * speed, rb.velocity.y);
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            sr.color = Color.red;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            sr.color = Color.blue;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            sr.color = Color.green;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        switch (collision.tag)
+        {
+            case "Death":
+                {
+                    string thislevel = SceneManager.GetActiveScene().name;
+                    SceneManager.LoadScene(thislevel);
+                    break;
+                }
+            case "Finish":
+                {
+                    SceneManager.LoadScene(nextlevel);
+                    break;
+                }
+        }
+
+    }
+}
